75eca7ffd62ca2d869e5e00da4ea0f05b1715909
AMRPH <shiidlovskiy@gmail.com>
2024-09-29 05:39:40 +0300

    fix

--- backend/controller.py
+++ backend/controller.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:4200", "http://localhost:80"],
+    allow_origins=["http://localhost:4200", "http://localhost:80", "http://astrum-team.ru:80"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
